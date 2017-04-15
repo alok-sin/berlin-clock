ffaa5ebef3a790dc9d59ca9da842afe73d40da5f
Alok Singh <Alok Singh>
2017-04-14 21:06:46 -0400

    Add project files.

---
+++ BerlinClock/Hello.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BerlinClock
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
