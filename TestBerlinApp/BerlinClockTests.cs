c86603175b287dae11f33bc452722261445a3c28
Alok Singh <alok.s09@gmail.com>
2017-04-17 23:25:47 -0400

    Simple test case build

---
+++ TestBerlinApp/BerlinClockTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BerlinClockApp;
+
+namespace TestBerlinApp
+{
+    [TestClass]
+    public class BerlinClockTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // arrange  
+            var timeConverter = new TimeConverterConcreteWithClock("extended", "berlin");
+
+            // act  
+            timeConverter.SetInputTime("10:45:32");
+            var actualTime = timeConverter.ShowClockTime();
+
+            // assert
+            Assert.AreEqual("Y RROO OOOO YYRYYRYYROO OOOO", actualTime, "Incorrect output");
+        }
+    }
+}
---
+++ TestBerlinApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestBerlinApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestBerlinApp")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("80507329-d3f7-468e-881b-4de7e9805009")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
